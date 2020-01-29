0d774762b7f9ef399fbe0b5548382bffbd0c54d9
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2020-01-29 19:20:33 +0100

    Initial commit

---
+++ src/__main__.py
@@ -0,0 +1,56 @@
+import mysql.connector
+import requests
+import argparse
+
+
+class Database:
+    def __init__(self, host):
+        self.__server = mysql.connector.connect(
+            host=host,
+            user="sensingStation",
+            passwd="8mUdOIU9I8Ws5Kc6",
+            database="sensingStation"
+        )
+
+    def __getCursor(self):
+        self.__cursor = self.__server.cursor(prepared=True)
+
+    def __saveData(self, signature, value: float):
+        sql = "INSERT INTO data (sensor_signature, value) VALUES (%s, %s)"
+        val = (signature, value)
+        self.__cursor.execute(sql, val)
+        self.__server.commit()
+
+    def saveTemperature(self, temperature: float):
+        self.__saveData("T1", temperature)
+
+    def saveHumidity(self, humidity: float):
+        self.__saveData("H1", humidity)
+
+
+class Satellite:
+    def __init__(self, url: str):
+        self.url = url
+
+    def getTemperature(self):
+        return requests.get(self.url).json()['data']['temperature']
+
+    def getHumidity(self):
+        return requests.get(self.url).json()['data']['humidity']
+
+
+def main():
+    parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+    parser.add_argument("-db", default="db", help="This is the 'a' variable")
+
+    args = parser.parse_args()
+
+    roof_sensor = Satellite('http://192.168.51.55')
+    server = Database(args.db)
+
+    server.saveTemperature(roof_sensor.getTemperature())
+    server.saveHumidity(roof_sensor.getHumidity())
+
+
+if __name__ == "__main__":
+    main()
